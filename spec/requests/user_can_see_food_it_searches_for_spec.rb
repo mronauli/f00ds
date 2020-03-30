0664832df4e1b66a37d9579734e0ec4c3e4241e9
Maria Ronauli <mronauli@gmail.com>
2020-03-30 10:16:16 -0600

    add test for user to see food

---
+++ spec/requests/user_can_see_food_it_searches_for_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+# When I visit "/"
+# And I fill in the search form with "sweet potatoes"
+# (Note: Use the existing search form)
+# And I click "Search"
+# Then I should be on page "/foods"
+# Then I should see a total of the number of items returned by the search.
+# Then I should see a list of ten foods that contain the ingredient "sweet potatoes"
+describe "as a user" do
+  it "can see search form for sweet potatoes"do
+    visit "/"
+
+    fill_in 'session[email]', with: "sweet potatoes"
+    click_on "Search"
+
+    expect(page).to have_content("50")
+    # translate fdcid to get food
+    expect(page).to have_content("600987")
+
+  end
+end
